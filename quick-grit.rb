fd9e5eae26cc91d5926e3c47b13c5e7a136cdec9
mcramm <GMCramm@gmail.com>
2010-03-26 14:14:19 -0600

    Add route/display patches of individual commits
    Navigating to /commit/:id will display the patch changes on that commit

--- quick-grit.rb
+++ quick-grit.rb
@@ -1,20 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'syntax/convertors/html'
 require 'erb'
 require 'grit'
 include Grit
 
-
+before do
+  @repo = Repo.new("/Users/gmichaelcramm/rails/repos/quick-grit")
+end
 
 get '/' do
-  @repo = Repo.new("/Users/gmichaelcramm/rails/repos/quick-grit")
   @commits = @repo.commits
   erb :index
 end
 
-get '/ticket/:id' do |id|
-  "Viewing Ticket #{id}"  # doesnt do anything yet!
+get '/commit/:id' do |id|
+  @commit = @repo.commits(id).first
+  erb :commit
 end
 
+
+def highlight(type, content)
+  converter = Syntax::Convertors::HTML.for_syntax type
+  @out = converter.convert(content)
+end
 
 private
