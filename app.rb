b30ae558b0a4036da3f853a3cef658d34412800c
mcramm <GMCramm@gmail.com>
2010-08-01 14:02:58 -0600

    Using hmal/sass now. Project name displayed.
    Project name will be displayed in title/header

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'rubygems'
+require 'compass'
+require 'coderay'
+require 'sinatra'
+require 'syntax/convertors/html'
+require 'haml'
+require 'grit'
+include Grit
+
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, "views"
+set :public, "static"
+
+configure do 
+    Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
+
+before do
+    repo_path = "/Users/gmichaelcramm/rails/repos/quick-grit"
+    @repo_name = repo_path.split("/").last
+    @repo = Repo.new(repo_path)
+end
+
+get '/stylesheets/:name.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
+
+get '/' do
+    @commits = @repo.commits
+    haml :index
+end
+
+get '/commit/:id' do |id|
+    @commit = @repo.commits(id).first
+    haml :commit
+end
+
+get '/blob/:id' do |id|
+    @blob = @repo.blob(id)
+    haml :blob
+end
+
+def highlight(type, content)
+    CodeRay.scan(content, 'diff').div(:css => :class)
+end
+
+private
